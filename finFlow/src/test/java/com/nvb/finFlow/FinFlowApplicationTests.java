904c2645269d006cf5ca18dba94129708c47a16e
nguyenvanbao1904 <bao19042004@gmail.com>
2025-07-25 10:42:21 +0700

    remove backend folder with spring mvc

---
+++ finFlow/src/test/java/com/nvb/finFlow/FinFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvb.finFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
